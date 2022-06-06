207e1f405d740ac2c7661e2b515a3c06a799aff5
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-06 16:32:59 -0700

    feat: complete insertionSort algo

--- client/insertionSort.js
+++ client/insertionSort.js
@@ -1,31 +1,50 @@
-function swap(arr, idx1, idx2) {
-  const temp = arr[idx1];
-  arr[idx1] = arr[idx2];
-  arr[idx2] = temp;
-}
+// function swap(arr, idx1, idx2) {
+//   const temp = arr[idx1];
+//   arr[idx1] = arr[idx2];
+//   arr[idx2] = temp;
+// }
+
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     for (let j = i; j > 0; j--) {
+//       if (arr[j] < arr[j - 1]) swap(arr, j, j - 1);
+//       else break;
+//     }
+//   }
+
+//   return arr;
+// }
 
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    for (let j = i; j > 0; j--) {
-      if (arr[j] < arr[j - 1]) swap(arr, j, j - 1);
-      else break;
+    const currElem = arr[i];
+
+    for (var j = i - 1; j >= 0 && arr[j] > currElem; j--) {
+      arr[j + 1] = arr[j];
     }
+
+    arr[j + 1] = currElem;
   }
 
   return arr;
 }
+
+export default insertionSort;
 
 /* ---------- PSEUDO ---------- */
 
 /*
 
 iterate through arr starting at index 1
+  declare a var called currElem init with the current element in loop
 
-  iterate through arr starting at curr index, going backwards while index is not 0
-    if the current element is SMALLER then the previos element
-      swap the two
-    otherwise
-      break
+  iterate through arr starting previous index, going backwards
+  (while index is greater then or equal to 0)
+  AND
+  (while current element in loop is greater then currElem)
+    swap elem at j with elem at j - 1
+
+  assign the element at j with currElem
 
 return the arr
 
