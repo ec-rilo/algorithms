92c466cd4c5b1c003cc7557c6546cb2850edb2b8
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-07 14:28:47 -0700

    feat: complete mergeSort

---
+++ client/mergeSort.js
@@ -0,0 +1,66 @@
+import merge from './merge';
+
+function mergeSort(arr) {
+  const traverseList = (list) => {
+    if (list.length === 1 || list.length === 0) {
+      return list;
+    }
+
+    const leftHalf = list.slice(0, Math.floor(list.length / 2));
+    const rightHalf = list.slice(Math.floor(list.length / 2));
+
+    const sortedLeftHalf = traverseList(leftHalf);
+    const sortedRightHalf = traverseList(rightHalf);
+
+    const sortedArr = merge(sortedLeftHalf, sortedRightHalf);
+
+    return sortedArr;
+  };
+
+  traverseList(arr);
+
+  return arr;
+}
+
+export default mergeSort;
+
+/* ---------- PSEUDO ---------- */
+
+/*
+
+declare func traverseList
+  if array length is 1 OR 0
+    return array
+
+  declare arr1 init with first half of input array
+  declare arr2 init with second half of input array
+
+  declare a var called firstHalf with invoke traverseList passing in firstHalf
+  declare a var called secondHalf with invoke traverseList passing in secondHalf
+
+  declare a var called sorted invoke merge passing in firstHalf and secondHalf
+
+  return sorted
+
+return traverseList passing in input arr
+
+*/
+
+/* ---------- IOCE ---------- */
+
+/*
+in:
+  - (array) - unsorted array of integers
+out:
+  - (array) - sorted array of integers
+cons:
+  - MUST use MERGE SORT
+edge:
+  - none
+*/
+
+/* ---------- TESTS ---------- */
+
+/*
+
+*/
